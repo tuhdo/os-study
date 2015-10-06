39fa87712f519caa318f995a0175d90f60ea8409
Tu Do <tu.h.do@dektech.com.au>
2015-10-06 15:03:49 +0700

    Finally return to correct code location
    We only use lower 16 bits, since we use segment in protected mode. This
    is necessary because INT instruction stores absolute return address,
    while IRET/IRETD uses segmented address.

--- kernel/interrupt_handler.asm
+++ kernel/interrupt_handler.asm
@@ -1,5 +1,8 @@
 bits 32
 
+cmp eax, 0
+je int_handler1
+
 cmp eax, 1
 je int_handler1
 
@@ -8,18 +11,27 @@ je int_handler1
 InterruptMsg  db "Interrupting", 0ah, 0h
 
 int_handler1:
-  mov		ax, 0x10		; set data segments to data selector (0x10)
-  mov		ds, ax
+  pusha
+  ; mov		ax, 0x10		; set data segments to data selector (0x10)
+  ; mov		ds, ax
 
-  mov bl, 5
-  mov bh, 5
-  call MovCur
+  ; mov bl, 5
+  ; mov bh, 5
+  ; call MovCur
 
-  mov eax, InterruptMsg
-  call Puts32
+  ; mov eax, InterruptMsg
+  ; call Puts32
 
-  ; restore back the stack for userspace afterward
-  mov ax, 0x23
-  mov ds, ax
+  ; ; restore back the stack for userspace afterward
+  ; mov ax, 0x23
+  ; mov ds, ax
+  popa
+
+  ; only use lower 16 bits, since we use segment in protected mode.
+  ; This is necessary because int instruction stores absolute address, while iret instruction use
+  ; segmented address.
+  mov eax, [esp]
+  and eax, 0xFFFF
+  mov [esp], eax
 
   iret
