b9286dc6adf3bbeb561480524b647160e488f487
Tu Do <tu.h.do@dektech.com.au>
2015-11-04 14:35:57 +0700

    [DONE] Routine to unset a bit in memory array
    [NEXT ]Create a function for initializing physical memory manager

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -102,15 +102,19 @@ Stage3:
 
   ; set block 9 in use
   mov eax, 9
-  call mem_set
+  call mem_set ; becomes 0x40 in mem_block_array index 1
 
   ; set block 8 in use
   mov eax, 8
-  call mem_set
+  call mem_set ; become 0xc0 in mem_block_array index 1
 
   ; unset block 9
   mov eax, 9
-  call mem_unset
+  call mem_unset ; become 0x40 in mem_block_array index 1
+
+  ; test block 8 set
+  mov eax, 8
+  call mem_test ; return 1 in eax
 
   call sysenter_setup
 
